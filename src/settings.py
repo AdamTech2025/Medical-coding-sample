6e168768ed1f9090e2b3dd98e608decb0b816ed0
yugandhar0609 <yugandharselvam@gmail.com>
2025-03-06 10:59:26 +0530

    Remove static file configuration for local development

--- src/settings.py
+++ src/settings.py
@@ -78,8 +78,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
